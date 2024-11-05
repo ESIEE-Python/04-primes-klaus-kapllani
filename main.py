a0f6de9b33d86d0ce922d6346e4c93a2aa5a7b61
klaus kapllani <klaus.kapllani@edu.esiee.fr>
2024-11-05 20:33:35 +0000

    Implémentation du code isprime

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
+# main.py
+"""
+author = klaus.kapllani@esiee.fr
+"""
+
 from math import sqrt
 
-#### Fonction secondaire
-
-
 def isprime(p):
+    """
+    Détermine si p est premier.
 
-    # votre code ici
+    Args : 
+        p : valeur entiere positive
 
-    pass
-
-#### Fonction principale
-
+    Returns :
+        isprime(p) : True si c'est un nombre premier, sinon False
+    """
+    # Si p est inférieur à 1, ce n'est pas un nombre premier
+    if p <= 1:
+        return False
+    # Sinon, on vérifie tous les diviseurs jusqu'à la racine carrée de n
+    for i in range(2, int(sqrt(p)) + 1):
+        if p%i == 0:
+            return False
+    return True
 
 def main():
+    """
+    Fonction principale qui affiche les nombres premier entre 1 et 100.
 
-    # vos appels à la fonction secondaire ici
-
-    for n in range(100):
-        if isprime(n):
-            print(n, end=", ")
-
-    print()
-
+    Returns :
+        rien
+    """
+    print("Nombres premiers entre 1 et 100 : ", [n for n in range(100) if isprime(n)])
 
 if __name__ == "__main__":
     main()
